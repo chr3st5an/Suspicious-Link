fea0a80903300ffc237aa8aec739397b38050985
chr3st5an <christiankreutz03@protonmail.com>
2022-04-26 00:08:36 +0200

    Added API

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ from typing import Tuple, Optional
 from random import choice, choices
 from datetime import datetime
 import logging
+import json
 import re
 import os
 
-from pymongo.collection import Collection
 from dotenv import load_dotenv
 from flask import (
     render_template,
@@ -36,12 +36,14 @@ __version__ = "1.0.0"
 
 load_dotenv()
 
-MONGO_URL    = os.getenv("MongoURL")
-SHADY_DOMAIN = os.getenv("ShadyDomain")
+MONGO_URL       = os.getenv("MongoURL")
+SHADY_DOMAIN    = os.getenv("ShadyDomain")
+DATABASE_NAME   = os.getenv("DatabaseName")
+COLLECTION_NAME = os.getenv("CollectionName")
 
 
 app     = Flask(__name__)
-mongodb = pymongo.MongoClient(MONGO_URL)["suspicious-link"]
+mongodb = pymongo.MongoClient(MONGO_URL)[DATABASE_NAME]
 
 
 @app.route("/")
@@ -60,6 +62,44 @@ def post() -> str:
     return redirect("/")
 
 
+@app.route("/api/create-link", methods=["POST"])
+def api_create_link() -> str:
+    """Represents an API to which POST request can be made
+
+    Example
+    -------
+    >>> from requests import post
+
+    >>> print(post("http://.../api/create-link", data={"url":"http://..."}))
+    """
+
+    response = {
+        "alias": None,
+        "error": None,
+        "code" : 201
+    }
+
+    request_is_invalid = not all([
+        request.form,
+        request.form.get("url"),
+        check_url_validity(request.form.get("url", ""))[0]
+    ])
+
+    if request_is_invalid:
+        response["code"]  = 400
+        response["error"] = "Hmmm, something is suspicious with the request"
+
+        return json.dumps(response), 400
+
+    url_alias, subpath = generate_link()
+
+    create_collection_entry(subpath, request.form["url"])
+
+    response["alias"] = url_alias
+
+    return json.dumps(response)
+
+
 @app.route("/post/create-link", methods=["GET", "POST"])
 def create_link_page() -> str:
     """Represents the directory which processes incoming form data
@@ -68,23 +108,16 @@ def create_link_page() -> str:
     if request.method != "POST":
         return redirect("/")
 
-    url: Optional[str] = request.form.get("url")
+    submitted_url: Optional[str] = request.form.get("url")
 
-    valid, error = check_url_validity(url)
+    valid, error = check_url_validity(submitted_url)
 
     if not valid:
         return render_template('index.html', title="index", error=error), 400
 
-    aliases_collection = mongodb["link-aliases"]
-    url_alias, path    = generate_link(aliases_collection)
+    url_alias, subpath = generate_link()
 
-    entry = {
-        "_id": path,
-        "alias": url,
-        "created_at": datetime.now(),
-    }
-
-    aliases_collection.insert_one(entry)
+    create_collection_entry(subpath, submitted_url)
 
     return render_template("index.html", title="index", alias_link=url_alias), 201
 
@@ -108,18 +141,20 @@ def redirect_suspicious_link(subpath: str) -> str:
     if request.method != "GET":
         return redirect(rick_roll, 303)
 
-    aliases_collection = mongodb["link-aliases"]
+    try:
+        url_args = list(*request.args.items())
+    except Exception:
+        return redirect(rick_roll, 303)
 
-    #> URL request parameters
-    args = list(request.args.items())
-
-    if len(args) != 2:
+    if len(url_args) != 2:
         return redirect(rick_roll, 303)
 
     #> Adding the request parameters to the subpath
-    subpath = f"{subpath}?{args[0]}={args[1]}"
+    subpath = f"{subpath}?{url_args[0]}={url_args[1]}"
 
-    entry = aliases_collection.find_one({"_id" : subpath})
+    link_collection = mongodb[COLLECTION_NAME]
+
+    entry = link_collection.find_one({"_id" : subpath})
 
     if entry is None:
         return redirect(rick_roll, 303)
@@ -133,6 +168,29 @@ def method_not_allowed(err) -> str:
     """
 
     return render_template("method_not_allowed.html"), 405
+
+
+def create_collection_entry(subpath: str, alias_for: str) -> None:
+    """Creates an entry in the link-collection
+
+    Parameters
+    ----------
+    subpath : str
+        The subpath that will later be used to retrieve
+        the alias
+    alias_for : str
+        The alias URL for the given subpath
+    """
+
+    link_collection = mongodb[COLLECTION_NAME]
+
+    link_collection.insert_one({
+        "_id": subpath,
+        "alias": alias_for,
+        "created_at": datetime.now()
+    })
+
+    return None
 
 
 def check_url_validity(url: Optional[str]) -> Tuple[bool, Optional[str]]:
@@ -162,13 +220,8 @@ def check_url_validity(url: Optional[str]) -> Tuple[bool, Optional[str]]:
     return (False if error else True), error
 
 
-def generate_link(collection: Collection) -> Tuple[str, str]:
+def generate_link() -> Tuple[str, str]:
     """Generates a suspicious link
-
-    Parameters
-    ----------
-    collection : pymongo.collection.Collection
-        Used to check if the link already exists
 
     Returns
     -------
@@ -178,11 +231,13 @@ def generate_link(collection: Collection) -> Tuple[str, str]:
         generated subpath.
     """
 
+    link_collection = mongodb[COLLECTION_NAME]
+
     subpath = choice(["", "-", "_"]).join(choices(DATA["words"], k=3)) + "?" + choice(DATA["parameters"])
 
     #> Check if the link is already in the database
-    if collection.find_one({"_id": subpath}):
-        return generate_link(collection)
+    if link_collection.find_one({"_id": subpath}):
+        return generate_link()
 
     return f"http://{SHADY_DOMAIN}/{subpath}", subpath
 
